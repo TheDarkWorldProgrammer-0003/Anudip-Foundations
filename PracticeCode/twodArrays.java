400d17c908a10972969196e71eebcd86e812c22b
Shrey Vashisth <143152184+TheDarkWorldProgrammer-0003@users.noreply.github.com>
2026-07-16 21:57:40 +0530

    Practice commit

---
+++ PracticeCode/twodArrays.java
@@ -0,0 +1,107 @@
+import java.util.*;
+
+public class twodArrays {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // int a [][] = {
+        // {1,2,3},
+        // {4,5,6},
+        // {7,8,9}
+        // };
+        // int b [][] = {
+        // {10,20,30},
+        // {40,50,60},
+        // {70,80,90} };
+        // int sum[][]= new int[a.length][b.length];
+        // for(int i =0 ; i< a.length;i++){
+        // for(int j =0 ;j < b.length; j++){
+        // sum[i][j] = a[i][j] + b[i][j];
+        // }
+        // System.out.println();
+        // }
+        // for(int i =0 ; i< a.length;i++){ // length of 2D array is decided by no. of
+        // rows ie 3 here
+        // for(int j =0 ;j < b.length; j++){
+        // System.out.println("The sum of 2D Array is :" + sum[i][j]);
+
+        // }
+        // }
+
+        // int a [][] = {
+        // {1,2,3},
+        // {4,5,6},
+        // {7,8,9}
+        // };
+        // int b [][] = {
+        // {10,20,30},
+        // {40,50,60},
+        // };
+        // int sum[][]= new int[a.length][b.length];
+        // for(int i =0 ; i< a.length;i++){
+        // for(int j =0 ;j < b.length; j++){
+        // sum[i][j] = a[i][j] + b[i][j];
+        // }
+        // System.out.println();
+        // }
+        // for(int i =0 ; i< a.length;i++){ // length of 2D array is decided by no. of
+        // rows ie 3 here
+        // for(int j =0 ;j < a[0].length; j++){ //it will decide the no. of column
+        // according to the elements in the a 0th Row i.e 3
+        // System.out.println("The sum of 2D Array is :" + sum[i][j]);
+
+        // }
+        // }
+        // Transpose of matrix
+       System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+
+        System.out.print("Enter the number of columns: ");
+        int col = sc.nextInt();
+
+        // Original matrix
+        int[][] a = new int[rows][col];
+        // Transpose matrix
+        int[][] transpose = new int[col][rows];   
+
+
+         // Input matrix
+        System.out.println("Enter the matrix elements:");
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < col; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < col; j++) {
+                transpose[j][i] = a[i][j];
+            }
+        }
+             // Print original matrix
+        System.out.println("\nOriginal Matrix:");
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        // Print transpose matrix
+        System.out.println("\nTranspose Matrix:");
+
+        for (int i = 0; i < col; i++) {
+            for (int j = 0; j < rows; j++) {
+                System.out.print(transpose[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+
+
+
+    }
+}
