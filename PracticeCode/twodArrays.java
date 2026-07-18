0a8435478b89698da1240552cb35044fee7e9c40
Shrey Vashisth <143152184+TheDarkWorldProgrammer-0003@users.noreply.github.com>
2026-07-18 18:22:33 +0530

    Update twodArrays.java

--- PracticeCode/twodArrays.java
+++ PracticeCode/twodArrays.java
@@ -53,90 +53,129 @@ public class twodArrays {
         // }
         // }
         // Transpose of matrix
-    //    System.out.print("Enter the number of rows: ");
-    //     int rows = sc.nextInt();
+        // System.out.print("Enter the number of rows: ");
+        // int rows = sc.nextInt();
 
-    //     System.out.print("Enter the number of columns: ");
-    //     int col = sc.nextInt();
+        // System.out.print("Enter the number of columns: ");
+        // int col = sc.nextInt();
 
-    //     // Original matrix
-    //     int[][] a = new int[rows][col];
-    //     // Transpose matrix
-    //     int[][] transpose = new int[col][rows];   
+        // // Original matrix
+        // int[][] a = new int[rows][col];
+        // // Transpose matrix
+        // int[][] transpose = new int[col][rows];
 
+        // // Input matrix
+        // System.out.println("Enter the matrix elements:");
 
-    //      // Input matrix
-    //     System.out.println("Enter the matrix elements:");
-
-    //     for (int i = 0; i < rows; i++) {
-    //         for (int j = 0; j < col; j++) {
-    //             a[i][j] = sc.nextInt();
-    //         }
-    //     }
-
-    //     for (int i = 0; i < rows; i++) {
-    //         for (int j = 0; j < col; j++) {
-    //             transpose[j][i] = a[i][j];
-    //         }
-    //     }
-    //          // Print original matrix
-    //     System.out.println("\nOriginal Matrix:");
-
-    //     for (int i = 0; i < rows; i++) {
-    //         for (int j = 0; j < col; j++) {
-    //             System.out.print(a[i][j] + " ");
-    //         }
-    //         System.out.println();
-    //     }
-
-    //     // Print transpose matrix
-    //     System.out.println("\nTranspose Matrix:");
-
-    //     for (int i = 0; i < col; i++) {
-    //         for (int j = 0; j < rows; j++) {
-    //             System.out.print(transpose[i][j] + " ");
-    //         }
-    //         System.out.println();
-    //     }
-
-    //     sc.close();
-
-
-
-    // Input array is : {10,20,30}
-        //              {40,50,60}
-                //      {70,80,90}
-
-// Print the primary diaonal of the array : {10,20,30}
-
-int b [][] = {
-        {10,20,30},
-        {40,50,60},
-        {70,80,90} };
-
-        // for(int i = 0;i<b.length;i++){
-        //     for(int j=0;j<b.length;j++){
-
-        //         if(i==j){
-        //             System.out.print(b[i][j] + " ");
-        //         }
-        //     }
+        // for (int i = 0; i < rows; i++) {
+        // for (int j = 0; j < col; j++) {
+        // a[i][j] = sc.nextInt();
+        // }
         // }
 
+        // for (int i = 0; i < rows; i++) {
+        // for (int j = 0; j < col; j++) {
+        // transpose[j][i] = a[i][j];
+        // }
+        // }
+        // // Print original matrix
+        // System.out.println("\nOriginal Matrix:");
 
+        // for (int i = 0; i < rows; i++) {
+        // for (int j = 0; j < col; j++) {
+        // System.out.print(a[i][j] + " ");
+        // }
+        // System.out.println();
+        // }
 
+        // // Print transpose matrix
+        // System.out.println("\nTranspose Matrix:");
 
-// Print the secondry  diaonal of the array : {30,50,70}
+        // for (int i = 0; i < col; i++) {
+        // for (int j = 0; j < rows; j++) {
+        // System.out.print(transpose[i][j] + " ");
+        // }
+        // System.out.println();
+        // }
 
- for(int i = 0;i<b.length;i++){
-            for(int j=0;j<b.length;j++){
+        // sc.close();
 
-                if (i + j == b.length - 1){
-                    System.out.print(b[i][j] + " ");
-                }
+        // Input array is : {10,20,30}
+        // {40,50,60}
+        // {70,80,90}
+
+        // Print the primary diaonal of the array : {10,20,30}
+
+        int b[][] = {
+                { 10, 20, 30 },
+                { 40, 50, 60 },
+                { 70, 80, 90 } };
+
+        // for(int i = 0;i<b.length;i++){
+        // for(int j=0;j<b.length;j++){
+
+        // if(i==j){
+        // System.out.print(b[i][j] + " ");
+        // }
+        // }
+        // }
+
+        // Print the secondry diaonal of the array : {30,50,70}
+
+        // for(int i = 0;i<b.length;i++){
+        // for(int j=0;j<b.length;j++){
+
+        // if (i + j == b.length - 1){
+        // System.out.print(b[i][j] + " ");
+        // }
+        // }
+        // }
+        // System.out.println();
+        // int even=0, odd =0;
+
+        // // Even and Odd no. in 2D Array
+        // for(int i = 0;i<b.length;i++){
+        // for(int j=0;j<b.length;j++){
+
+        // if(b[i][j] % 2 == 0){
+        // even ++;
+
+        // }
+        // else{
+        // odd++;
+
+        // }
+
+        // }
+        // }
+        // System.out.println( "Number of even no.in given matrix =" + even + " Number
+        // of even no.in given matrix =" + odd);
+
+        // To find the sum of every row and column
+
+        int m = b.length, n = b[0].length;
+        int sume = 0, sumo = 0;
+
+        // row sum
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                sume += b[i][j];
+
             }
+            System.out.println("Sum of row " + i + " = " + sume);
         }
 
+        //  column sums
+        for (int j = 0; j < n; j++) {
+
+            for (int i = 0; i < m; i++) {
+                sumo += b[i][j];
+            }
+            System.out.println("Sum of col " + j + " = " + sumo);
+
+        }
 
     }
 }
